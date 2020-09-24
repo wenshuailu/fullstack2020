3e5edf67ba79c030bb473fad25dbb56311276a70
Claire Lu <wla137@sfu.ca>
2020-09-24 00:23:40 -0700

    part2 c

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,78 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 // import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
+// const Header = (props) => {
+//   return <h1>{props.course}</h1>;
+// };
 
 
-const Content = (props) => {
+// const Content = (props) => {
+//   return (
+//     <div>
+//       <Part {...props.parts[0]} />
+//       <Part {...props.parts[1]} />
+//       <Part {...props.parts[2]} />
+//     </div>
+//   );
+// };
+
+// const Part = (props) => {
+//   // console.log(props);
+//   return (
+//     <p>
+//       {props.name} {props.exercises}
+//     </p>
+//   );
+// };
+
+// const Total = (props) => {
+//   const total = props.parts.map(part => part.exercises).reduce((a, c) => a+c);
+//   return <p>Number of exercises {total}</p>;
+// };
+
+// const App = () => {
+//   const course = {
+//     name: 'Half Stack application development',
+//     parts: [
+//       {
+//         name: 'Fundamentals of React',
+//         exercises: 10
+//       },
+//       {
+//         name: 'Using props to pass data',
+//         exercises: 7
+//       },
+//       {
+//         name: 'State of a component',
+//         exercises: 14
+//       }
+//     ]
+//   }
+
+//   const {name, parts} = course;
+  
+//   return (
+//     <div>
+//       <Header course={name} />
+//       <Content parts={parts} />
+//       <Total parts={parts} />
+//     </div>
+//   );
+// };
+const Display = (props) => {
   return (
-    <div>
-      <Part {...props.parts[0]} />
-      <Part {...props.parts[1]} />
-      <Part {...props.parts[2]} />
-    </div>
-  );
-};
+    <div>{props.counter}</div>
+  )
+}
 
-const Part = (props) => {
-  // console.log(props);
+const Button = (props) => {
   return (
-    <p>
-      {props.name} {props.count}
-    </p>
-  );
-};
-
-const Total = (props) => {
-  return <p>Number of exercises {props.exercises}</p>;
-};
+    <button onClick = {props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const [ counter, setCounter ] = useState(0);
 
-  const parts = [
-    {
-      name: part1,
-      count: exercises1,
-    },
-    {
-      name: part2,
-      count: exercises2,
-    },
-    {
-      name: part3,
-      count: exercises3,
-    },
-  ];
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total exercises={exercises1 + exercises2 + exercises3} />
-      {/* <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
+      <Display counter={counter} />
+      <Button        
+        handleClick={increaseByOne}        
+        text='plus'      
+      />      
+      <Button        
+        handleClick={setToZero}        
+        text='zero'      
+      />           
+      <Button        
+        handleClick={decreaseByOne}        
+        text='minus'      
+      />    
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
